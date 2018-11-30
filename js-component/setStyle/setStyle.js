76f4fb125527ea0d9be40bf482dadad959e3aef6
LIMENGYA <myjoyce817@163.com>
2018-11-30 14:06:20 +0800

    重置样式

---
+++ js-component/setStyle/setStyle.js
@@ -0,0 +1,42 @@
+(function(win,doc){
+    var defaultSettings = {
+        color:"red",
+        background:"blue",
+        border:"2px solid #000",
+        fontSize:"30px",
+        textSlign:"center",
+        width:"200px",
+        borderRadius:"5px"
+    };
+    function SetStyles(options){
+        var self = this;
+        //没传配置项会报错
+        if(!options){
+            throw new Error("请传配置参数");
+        }
+        self = Object.assign(self,defaultSettings,options);
+
+        self.container = doc.querySelector(self.container) || doc.querySelectorAll(self.container);
+        self._changeStyles();
+    }
+
+    SetStyles.prototype = {
+        _changeStyles:function(){
+            var self = this;
+            for(var pro in self){
+                if(pro == "container"){
+                    continue;
+                }
+                if(pro == "text" && typeof self[pro]=='string'){
+                    self.container.innerText = self[pro];
+                    continue;
+                }else if(pro == 'text' && typeof self[pro]=='function'){
+                    self.container.innerText = self[pro]();
+                    continue;
+                }
+                self.container.style[pro] = self[pro];
+            }
+        }
+    }
+    win.SetStyles = SetStyles;
+})(window,document)
